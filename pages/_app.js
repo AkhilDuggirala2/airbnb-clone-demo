b25536144af1e66b717a29415905ffb0daed38c3
Your Name <you@example.com>
2022-01-27 11:01:39 +0530

    day two build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,19 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.css';
+import Router from "next/router";
+
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#FE595E',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on("routerChangeStart", progress.start);
+Router.events.on("routerChangeComplete", progress.finish);
+Router.events.on("routerChangeError", progress.finish);
 
 
 function MyApp({ Component, pageProps }) {
